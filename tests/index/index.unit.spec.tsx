0972d2c32129fcdc11b467cd9c4d02117fff4737
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-03-31 23:47:07 +0300

    Add: fetch listings test

--- tests/index/index.unit.spec.tsx
+++ tests/index/index.unit.spec.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import sinon from "sinon";
+import sinon, { SinonStub } from "sinon";
 import faker from "faker";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import Home from "../../pages/index";
 import { BottomBar } from "../../components/bottom-bar";
 import { getPropsOfCallByComponent } from "../utils";
+import { api } from "../../common/constant";
 
 const sandbox = sinon.createSandbox();
 const { random: { number }, lorem: { word, words } } = faker;
 
 describe("Home Page Unit Tests", () => {
   beforeEach(() => {
-    window.fetch = sandbox.stub().returns({ok: true, json: sandbox.stub().resolves([])});
+    window.fetch = sandbox.stub().resolves({ ok: true, json: sandbox.stub().resolves([]) });
   });
   afterEach(() => {
     sandbox.verifyAndRestore();
@@ -43,5 +44,20 @@ describe("Home Page Unit Tests", () => {
     // Assert
     expect(eventStub.called).toBe(true);
 
+  });
+
+  it("should fetch listings", () => {
+    // Arrange
+    sandbox.spy(React, "createElement");
+    const eventStub = sandbox.stub();
+
+    // Act
+    const { container, debug } = render(<Home />);
+    const spy = React.createElement as any;
+    getPropsOfCallByComponent(spy, BottomBar).onBuyClick({ stopPropagation: eventStub });
+
+    // Assert
+    expect(eventStub.called).toBe(true);
+    expect((window.fetch as SinonStub).calledWithExactly(`${api.mobile}/listings`, {method: 'GET'}))
   });
 });
