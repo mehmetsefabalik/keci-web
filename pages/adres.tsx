347baf53ebf56af60edede157d181492e9ecd87b
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-04-15 23:34:40 +0300

    Add: adres page is added

---
+++ pages/adres.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { makeStyles, Grid } from "@material-ui/core";
+import Router from "next/router";
+
+const useStyles = makeStyles({});
+
+const Adres = () => {
+  return (
+    <>
+      <Grid container alignItems="center" justify="center">
+        Adres
+      </Grid>
+    </>
+  );
+};
+
+export default Adres;
